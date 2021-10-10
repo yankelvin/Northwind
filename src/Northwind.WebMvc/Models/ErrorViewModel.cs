fc798adaabac8f43663602a5cd49b550e34baf5b
Yan Kelvin <yan.kelvin@hotmail.com>
2021-10-09 21:26:59 -0300

    Add project files.

---
+++ src/Northwind.WebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Northwind.WebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
